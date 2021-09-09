dec74555646853a67d03c435f2800d60b7f53d35
MClarke93 <mjclarke1993@gmail.com>
2021-09-09 12:43:45 +1000

    Finished low-level db api

--- data.py
+++ data.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 '''The script that populates the CouchDB database with test data.'''
 
 import sys
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python3
 '''The script that starts the main DEHC application.'''
 
 import sys
+from mods.database import DEHCDatabase
+
+db = DEHCDatabase(config="database.json", loud=True)
+db.create_databases()
+db.delete_databases()
 
 sys.exit(0)
